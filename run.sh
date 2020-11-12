ca8f5ba97d080d5a8c0d544dd581bda1843e312b
Rollingegg <1244797125@qq.com>
2020-11-12 18:22:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 APP_DIR="legacySystem-webService"
 PROCESS_NAME=oldCompanySystem
 kill -9 $(ps -eaf|grep -i "java .*-classpath .*${PROCESS_NAME}"|grep -v 'grep'|awk '{print $2}')
-echo `"Closing" ${PROCESS_NAME}`
+echo `Closing ${PROCESS_NAME}`
 git pull
 cd ${APP_DIR}
 nohup mvn org.springframework.boot:spring-boot-maven-plugin:2.2.4.RELEASE:run 2> ${PROCESS_NAME}.log
-echo `${PROCESS_NAME} "Started"`
+echo `${PROCESS_NAME} Started`
